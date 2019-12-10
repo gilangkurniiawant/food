f87a65989c0afb118d7f0f0691b98344dc96bea5
gilangkurniiawant <gkurniiawant@gmail.com>
2019-12-10 12:13:41 +0700

    Lolo

--- curl.php
+++ curl.php
@@ -6,12 +6,12 @@ $header[] = "User-Agent: okhttp/3.10.0";
 $header[] = "Accept: application/json";
 $header[] = "Accept-Language: en-ID";
 $header[] = "Content-Type: application/json; charset=UTF-8";
-$header[] = "X-AppVersion: 3.30.2";
+$header[] = "X-AppVersion: 3.41.1";
 $header[] = "X-UniqueId: ".time()."57".mt_rand(1000,9999);
 $header[] = "Connection: keep-alive";
 $header[] = "X-User-Locale: en_ID";
 $header[] = "X-Location: -3.131498,98.911".mt_rand(1000,9999);
-$header[] = "X-Location-Accuracy: 3.0";
+$header[] = "X-Location-Accuracy: 1.0";
 if ($pin):
 $header[] = "pin: $pin";
     endif;
